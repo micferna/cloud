8568decab55942bc248db4b1091f2e59a0abebcb
micferna <122208805+micferna@users.noreply.github.com>
2023-12-11 23:54:48 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,63 +1,41 @@
 #!/bin/sh
-# en root
 
 # Mise à jour des paquets
 apt update && apt upgrade -y
-if [ $? -eq 0 ]; then
-    echo "Mise à jour réussie."
-else
-    echo "Échec de la mise à jour."
-    exit 1
-fi
 
 # Installation de cloud-init
 apt install -y cloud-init
-if [ $? -eq 0 ]; then
-    echo "Installation de cloud-init réussie."
+
+# Configuration de SSH pour autoriser la connexion en tant que root
+SSH_CONFIG="/etc/ssh/sshd_config"
+sed -i "s/PermitRootLogin prohibit-password/PermitRootLogin yes/g" "$SSH_CONFIG"
+
+# Sauvegarde et modification de la configuration réseau
+NETWORK_CONFIG="/etc/network/interfaces"
+cp "$NETWORK_CONFIG" "${NETWORK_CONFIG}old"
+sed -n '1,4p' "${NETWORK_CONFIG}old" > "$NETWORK_CONFIG"
+
+# Sauvegarde et modification de la configuration de cloud-init
+CLOUD_CFG="/etc/cloud/cloud.cfg"
+cp "$CLOUD_CFG" "${CLOUD_CFG}old"
+sed -i "s/disable_root: true/disable_root: false/g" "${CLOUD_CFG}old"
+sed -n '1,/distro: debian/p; /# Other config here will be given to the distro class and\/or path classes/,$p' "${CLOUD_CFG}old" > "$CLOUD_CFG"
+
+# Redémarrage du service SSH pour appliquer les changements
+systemctl restart sshd
+
+# Vérification de la possibilité de se connecter en tant que root via SSH
+if grep -q "PermitRootLogin yes" "$SSH_CONFIG"; then
+    echo "Connexion SSH en tant que root autorisée."
+
+    # Suppression de l'utilisateur 'user' si spécifié
+    USERNAME_TO_DELETE="user"
+    if id "$USERNAME_TO_DELETE" &>/dev/null; then
+        deluser --remove-home "$USERNAME_TO_DELETE"
+        echo "Utilisateur '$USERNAME_TO_DELETE' supprimé."
+    else
+        echo "Utilisateur '$USERNAME_TO_DELETE' non trouvé."
+    fi
 else
-    echo "Échec de l'installation de cloud-init."
-    exit 1
+    echo "Erreur : La configuration SSH pour la connexion root n'a pas été mise à jour correctement."
 fi
-
-# Modification de sshd_config
-sed -i "s/PermitRootLogin prohibit-password/PermitRootLogin yes/g" "/etc/ssh/sshd_config"
-if grep -q "PermitRootLogin yes" "/etc/ssh/sshd_config"; then
-    echo "Modification de sshd_config réussie."
-else
-    echo "Échec de la modification de sshd_config."
-    exit 1
-fi
-
-# Modification de /etc/network/interfaces
-mv /etc/network/interfaces /etc/network/interfacesold
-sed -n '1,4p' "/etc/network/interfacesold" > "/etc/network/interfaces"
-if [ $? -eq 0 ]; then
-    echo "Modification de /etc/network/interfaces réussie."
-else
-    echo "Échec de la modification de /etc/network/interfaces."
-    exit 1
-fi
-
-# Modification de cloud.cfg
-mv /etc/cloud/cloud.cfg /etc/cloud/cloudold.cfg
-sed -i "s/disable_root: true/disable_root: false/g" "/etc/cloud/cloudold.cfg"
-cp /etc/cloud/cloudold.cfg /etc/cloud/cloud.cfg
-if [ $? -eq 0 ]; then
-    echo "Modification de cloud.cfg réussie."
-else
-    echo "Échec de la modification de cloud.cfg."
-    exit 1
-fi
-
-# Nettoyer /var/log
-echo "Nettoyage des fichiers de log dans /var/log..."
-sudo sh -c 'rm -rf /var/log/*'
-
-# Nettoyer les journaux de systemctl
-echo "Nettoyage des journaux de systemctl..."
-sudo journalctl --rotate
-sudo journalctl --vacuum-time=1s
-
-echo "Nettoyage terminé."
-
-echo "Toutes les étapes ont été exécutées avec succès."
